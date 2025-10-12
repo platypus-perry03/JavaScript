18da995e1476acdc2f1d734ad4ea0f09ef7f8f88
platypus-perry03 <choe31904@gmail.com>
2025-10-12 10:56:56 +0900

    feat: Initialize game state and character variables

--- Dino_run/public/JS/index.js
+++ Dino_run/public/JS/index.js
@@ -1,3 +1,4 @@
+// DOM
 const game = document.getElementById("game");
 const ground = document.getElementById("ground");
 const clouds = document.getElementById("clouds");
@@ -6,3 +7,13 @@ const block = document.getElementById("block");
 const overlay = document.getElementById("overlay");
 const scoreEl = document.getElementById("score");
 const hiEl = document.getElementById("hi");
+
+// 상태
+const GAME = { READY: 0, PLAYING: 1, OVER: 2};
+let state = GAME.READY;
+
+let y = 0;                  // 캐릭터 높이 (바닥 기준)
+let vy = 0;                 // 속도
+const GRAVITY = -0.0032;    // 중력
+const JUMP_V = 0.08;        // 점프
+let last = 0
